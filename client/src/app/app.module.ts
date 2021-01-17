f10960fed0885115ee3d3e09feaacb5f5583f124
Christian Charles Mercado <mercadochristiancharles@gmail.com>
2021-01-17 23:20:44 +0800

    End of section 5

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -4,17 +4,29 @@ import {HttpClientModule} from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NavComponent } from './nav/nav.component';
+import { FormsModule } from '@angular/forms';
+import { HomeComponent } from './home/home.component';
+import { RegisterComponent } from './register/register.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,
+    HomeComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
-    BrowserAnimationsModule
+    BsDropdownModule.forRoot(),
+    BrowserAnimationsModule,
+    FormsModule,
+   
   ],
   providers: [],
   bootstrap: [AppComponent]
