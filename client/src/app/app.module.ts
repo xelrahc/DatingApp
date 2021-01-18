81ae60548d1ffb425d9e51d82e0d448901dd4c3f
Christian Charles Mercado <mercadochristiancharles@gmail.com>
2021-01-18 10:09:35 +0800

    End of section 6

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -10,6 +10,12 @@ import { NavComponent } from './nav/nav.component';
 import { FormsModule } from '@angular/forms';
 import { HomeComponent } from './home/home.component';
 import { RegisterComponent } from './register/register.component';
+import { MemberListComponent } from './members/member-list/member-list.component';
+import { MemberDetailComponent } from './members/member-detail/member-detail.component';
+import { ListsComponent } from './lists/lists.component';
+import { MessagesComponent } from './messages/messages.component';
+import { ToastrModule } from 'ngx-toastr';
+import { SharedModule } from './_modules/shared.module';
 
 
 @NgModule({
@@ -17,16 +23,19 @@ import { RegisterComponent } from './register/register.component';
     AppComponent,
     NavComponent,
     HomeComponent,
-    RegisterComponent
+    RegisterComponent,
+    MemberListComponent,
+    MemberDetailComponent,
+    ListsComponent,
+    MessagesComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
-    BsDropdownModule.forRoot(),
     BrowserAnimationsModule,
     FormsModule,
-   
+    SharedModule
   ],
   providers: [],
   bootstrap: [AppComponent]
